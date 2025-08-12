44c2da6c0fb3d948923e4516d6e1e13adc1a062e
GregoryGapuz <gvgapuz82675@liceo.edu.ph>
2025-08-12 10:00:41 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printly("Hello World!");
+        Book book1 = new Book("Greatest", "Lebby", 100);
+        Book book2 = new Book("Goat", "Micmic", 464);
+        Book book3 = new Book("wizard", "Thomas", 352);
+
+        book1.displayInfo();
+        book2.displayInfo();
+        book3.displayInfo();
+
+        book1.borrowBook();
+        book2.borrowBook();
+
+        book1.displayInfo();
+        book2.displayInfo();
+
+        book1.returnBook();
+        book1.displayInfo();
     }
 }
