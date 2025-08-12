b88dd0543939e8b87883acdad692a910f92d6edd
GregoryGapuz <gvgapuz82675@liceo.edu.ph>
2025-08-12 10:06:11 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printly("Hello World!");
+        Student s1 = new Student("Greg Gapuz", 20, "OOP", 80, 75, 80);
+        Student s2 = new Student("Karl Veluz", 19, "PUR-COM", 85, 70, 85);
+        Student s3 = new Student("Kenny Omega", 18, "STS", 70, 65, 68);
+
+        Student[] students = {s1, s2, s3};
+        int passingCount = 0;
+
+        for (Student s : students) {
+            s.displayInfo();
+            double avg = s.calculateAverage();
+            System.out.printf("Average: %.2f\n", avg);
+            String letter = s.getLetterGrade();
+            System.out.println("Letter Grade: " + letter);
+            boolean passing = s.isPassing();
+            System.out.println("Status: " + (passing ? "PASSING" : "FAILING"));
+            System.out.println();
+            if (passing) passingCount++;
+        }
+
+        System.out.println("Summary: Total number of students who are passing: " + passingCount);
     }
 }
